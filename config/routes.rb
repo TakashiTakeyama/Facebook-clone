fc21887aa1f06e29387a216fc84e09c2d1ff5d00
Takashi <Takashi@gmail.com>
2019-07-11 10:21:49 +0900

    sessionsコントロラーを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root to:'blogs#index'
   resources :blogs
   resources :users, only:[:new,:create,:show]
+  resources :sessions, only:[:new,:create,:destroy]
 end
